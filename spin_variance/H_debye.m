6aac63106660fd771e0dfbb12281a17d75dce9eb
Ethan Andersen <etan5063@colorado.edu>
2020-06-11 21:59:03 -0600

    finished the coupling matrix

--- spin_variance/H_debye.m
+++ spin_variance/H_debye.m
@@ -19,6 +19,9 @@ for ii = 1:n_bath
         my_ket(jj) = rem;
         dec = quo;
     end
+    
+    %---------------raising operator start------------------------%
+        % a^dag |n> = sqrt(1/(2w)) sqrt((n+1)%mode_count) |n+1>
     ket_prefactors = sqrt((bath_terms(2,:).* mod(my_ket+1,mode_count))./(2.*bath_terms(1,:)));
     %new_kets = repelem(my_ket,osc_count,1) + diag(ones(1,osc_count));
     
@@ -34,8 +37,20 @@ for ii = 1:n_bath
     end
     
     
-       
-    %given the oscillator k, a^dag |n> = sqrt((n+1)%mode_count)
+    %--------------lowering operator start------------------------%
+        % a |n> = sqrt(1/(2w)) sqrt((n)) |n-1>
+    ket_prefactors = sqrt(bath_terms(2,:)./(2.*bath_terms(1,:))).*sqrt(my_ket);
+    new_indices = ii -mode_count.^(0:(osc_count-1));
+    for jj = 1:osc_count
+        prefactor = ket_prefactors(jj);
+        row = new_indices(jj);
+        col = ii;
+        if(prefactor ~= 0)
+            H_bc(row,col) = prefactor;
+            H_bc(row + n_bath, col + n_bath) = -prefactor;
+        end
+    end
+    
     
     
 
