66b2770a302e985d45ba39581ce6b5357380e300
Ethan Andersen <etan5063@colorado.edu>
2020-06-19 00:55:25 -0600

    more progress on tensor version

--- refresh_diff_evolve/psi_naught.m
+++ refresh_diff_evolve/psi_naught.m
@@ -1,44 +1,47 @@
-function psi = psi_naught(bath_terms,spin_state, beta)
+function psi = psi_naught(bath_terms,beta,spin_state)
 
 mode_counts = bath_terms(3,:);
 omega_vec = bath_terms(1,:);
 osc_count = length(omega_vec);
-n_bath_terms = prod(mode_counts);
+bath_count = prod(mode_counts);
 
 %construct the bath states
-psi = zeros(1,n_bath_term);
+psi = zeros(bath_count,1);
+psi_last = zeros(bath_count,1);
 counter_vec = zeros(1,osc_count);
-for ii = 1:n_bath_terms
+for ii = 1:(bath_count-1)
      %increment counter by one and then iter. through terms
      counter_vec(1) = counter_vec(1) + 1;
     for jj = 1:1:osc_count
-      if mod(counter_vec(jj),mode_count(jj)) == 0
+      if counter_vec(jj)== mode_counts(jj)
           counter_vec(jj) = 0;
           counter_vec(jj+1) = counter_vec(jj+1) +1;
           
       end 
     end
     counter_vec
-    tmp_state = zeros(mode_counts(jj),1);
-    tmp_state(counter_vec(jj)) = 1;
-    tmp_energy = bath_terms(1,jj).*(counter_vec+1/2);
+    %prepare for direct product of osc1*(osc2*(...*oscn))
+    tmp_state = zeros(mode_counts(end),1);
+    tmp_state(counter_vec(end)+1) = 1;
+    tmp_energy = bath_terms(1,end).*(counter_vec(end)+1/2);
     therm_prob = exp(-beta.*tmp_energy);
     tmp_state = sqrt(therm_prob).*tmp_state;
     psi = tmp_state;
-    for jj = 2:1:osc_count
+    for jj = (osc_count-1):-1:1
         
         tmp_state = zeros(mode_counts(jj),1);
-        tmp_state(counter_vec(jj)) = 1;
-        tmp_energy = bath_terms(1,jj).*(counter_vec+1/2);
+        tmp_state(counter_vec(jj)+1) = 1;
+        tmp_energy = bath_terms(1,jj).*(counter_vec(jj)+1/2);
         therm_prob = exp(-beta.*tmp_energy);
         tmp_state = sqrt(therm_prob).*tmp_state;
         
         psi = kron(tmp_state,psi);
     end
+    psi_last = psi_last + psi;
 
 end
 
-psi = 1/norm(psi).*psi;
+psi = 1/norm(psi_last).*psi_last;
 psi = kron(spin_state,psi);
 
 end
